41023b7439f6c12e25e4ac1c4f178ea3a119f1e8
Joe Martella <martellaj@live.com>
2015-04-14 19:17:27 -0700

    Added the back-end.

--- front-end/app.routes.js
+++ front-end/app.routes.js
@@ -5,19 +5,19 @@ angular.module('routes', ['ngRoute'])
 	$routeProvider
 
 		.when('/', {
-			templateUrl: 'front-end/views/view1/view1.html',
+			templateUrl: 'views/view1/view1.html',
 			controller: 'view1Controller',
 			controllerAs: 'view1'
 		})
 
 		.when('/view1', {
-			templateUrl: 'front-end/views/view1/view1.html',
+			templateUrl: 'views/view1/view1.html',
 			controller: 'view1Controller',
 			controllerAs: 'view1'
 		})
 
 		.when('/view2', {
-			templateUrl: 'front-end/views/view2/view2.html',
+			templateUrl: 'views/view2/view2.html',
 			controller: 'view2Controller',
 			controllerAs: 'view2'
 		})
---
+++ server.js
@@ -0,0 +1,39 @@
+var express   	= require('express');
+var app        	= express();
+var mongoose		= require('mongoose'); 
+var morgan 			= require('morgan');
+var path 				= require('path'); 
+
+// Initialize variables. 
+var port = process.env.PORT || 8080; 
+
+// Connect to DB.
+// mongoose.connect(dbConnectionString);
+
+// Configure morgan module to log all requests.
+app.use(morgan('dev')); 
+
+// Set the front-end folder to serve public assets.
+app.use(express.static(__dirname + '/front-end'));
+
+// Configure app to handle CORS requests.
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, \Authorization');
+
+	next();
+});
+
+// Route registration. 
+// var apiRouter = require('./app/routes/apiRouter');
+// app.use('/api', apiRouter); 
+
+// Set up our one route to the index.html file.
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/views/index.html'));
+});
+
+// Start the app.  
+app.listen(port);
+console.log('Listening on port ' + port + '...'); 
