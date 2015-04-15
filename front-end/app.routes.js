88322f937604755231c92f4593eb8151a309e821
Joe Martella <martellaj@live.com>
2015-04-14 17:52:51 -0700

    Separated routing.

--- front-end/app.routes.js
+++ front-end/app.routes.js
@@ -5,19 +5,19 @@ angular.module('routes', ['ngRoute'])
 	$routeProvider
 
 		.when('/', {
-			templateUrl: 'front-end/view1/view1.html',
+			templateUrl: 'front-end/views/view1/view1.html',
 			controller: 'view1Controller',
 			controllerAs: 'view1'
 		})
 
 		.when('/view1', {
-			templateUrl: 'front-end/view1/view1.html',
+			templateUrl: 'front-end/views/view1/view1.html',
 			controller: 'view1Controller',
 			controllerAs: 'view1'
 		})
 
 		.when('/view2', {
-			templateUrl: 'front-end/view2/view2.html',
+			templateUrl: 'front-end/views/view2/view2.html',
 			controller: 'view2Controller',
 			controllerAs: 'view2'
 		})
