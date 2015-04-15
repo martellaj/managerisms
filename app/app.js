7f887a7008a1f4cc33a60b16fb983a8cb27a4e02
Joe Martella <martellaj@live.com>
2015-04-14 17:11:57 -0700

    Routing not working.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,30 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+])
+
+.config(['$routeProvider', function($routeProvider, $locationProvider) {
+  $routeProvider
+
+		.when('/view1', {
+			templateUrl: 'views1/view1.html',
+			controller: 'view1Controller',
+			controllerAs: 'view1'
+		})
+
+		.when('/view2', {
+			templateUrl: 'app/views2/view2.html',
+			controller: 'view2Controller',
+			controllerAs: 'view2'
+		})
+
+		// Send all other requests to root.
+		.otherwise({ redirectTo: '/' });
+
+		// Make URLs pretty.
+		$locationProvider.html5Mode(true); 
 }]);
