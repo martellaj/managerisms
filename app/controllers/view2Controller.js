74436ae66751642432cf8caba6ef1da0833aad8b
Joe Martella <martellaj@live.com>
2015-05-26 15:55:56 -0700

    Added activate functions to the controllers.

--- app/controllers/view2Controller.js
+++ app/controllers/view2Controller.js
@@ -2,7 +2,27 @@
 	angular.module('seedApp')
 	.controller('View2Controller', View2Controller);
 	
-	function View2Controller() {
+	// Inject dependencies to protect app from minification.
+	View2Controller.$inject = ['$log'];
+	
+	/**
+	 * The View2Controller code.
+	 */
+	function View2Controller($log) {
+		var vm = this;
 		
+		/////////////////////////////////////////
+		// End of exposed properties and methods.
+		
+		// Activate controller when it loads.
+		activate();
+		
+		/**
+		 * This function does any initialization work the 
+		 * controller needs.
+		 */
+		function activate() {
+			$log.debug('Activated View2Controller.');
+		};
 	};
 })();
