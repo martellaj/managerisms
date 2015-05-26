362fc784ffae18893ed2be6184f628c1c8bf209d
Joe Martella <martellaj@live.com>
2015-05-26 15:46:06 -0700

    Added some meat to a controller.

--- app/controllers/view1Controller.js
+++ app/controllers/view1Controller.js
@@ -3,6 +3,22 @@
 	.controller('View1Controller', View1Controller);
 	
 	function View1Controller() {
+		var vm = this;
 		
+		// Properties
+		vm.name = 'Joe';
+		vm.greeting = ''; 
+		
+		// Methods
+		vm.greet = greet;
+		
+		////////////////////////////////
+		
+		/**
+		 * This function builds a greeting.
+		 */
+		function greet() {
+			vm.greeting = 'Hello, ' + vm.name + '!'; 
+		};
 	};
 })();
