31c6b8b3fddf0b3dbd6cb2744cefe6b672792a33
Joe Martella <martellaj@live.com>
2015-11-19 10:11:38 -0800

    Added a dependency.

--- app.js
+++ app.js
@@ -24,10 +24,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-// View engine setup.
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // Uncomment after placing your favicon in /public.
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
