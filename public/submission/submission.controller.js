34eb58c93b5a6f297538e55d3b94e6139c2a499a
Joe Martella <martellaj@live.com>
2015-11-22 15:17:26 -0800

    Submission UI done.

--- public/submission/submission.controller.js
+++ public/submission/submission.controller.js
@@ -5,9 +5,16 @@
     .module('app.submission')
     .controller('SubmissionController', SubmissionController);
 
-  SubmissionController.$inject = ['$log'];
-  function SubmissionController ($log) {
+  SubmissionController.$inject = ['$log', '$http'];
+  function SubmissionController ($log, $http) {
     var vm = this;
+
+    // Properties
+    vm.quote;
+    vm.company;
+
+    // Methods
+    vm.submit = submit;
 
     // *********************************** //
     // End of exposed properties and methods.
@@ -15,5 +22,29 @@
     (function activate () {
       $log.debug('Activated SubmissionController.');
     })();
+
+    function submit () {
+      $log.debug('Form submit handler called.');
+
+      var req = {
+        method: 'POST',
+        url: '/managerism/submit',
+        data: {
+          quote: vm.quote,
+          company: vm.company
+        }
+      };
+
+      // $http(req)
+      //   .then(function (res) {
+      //     displayMessage('Just shouted this managerism into the void.');
+      //   }, function (err) {
+      //     if (err === 400) {
+      //       displayMessage('This managerism is too long to tweet.');
+      //     } else {
+      //       displayMessage('Something went wrong.');
+      //     }
+      //   });
+    }
   }
 })();
--- public/submission/submission.module.js
+++ public/submission/submission.module.js
@@ -1,5 +1,5 @@
 (function () {
   'use strict';
 
-  angular.module('app.submission', []);
+  angular.module('app.submission', ['ngMessages']);
 })();
